1a6a790227af9a60e710c616961247f866dabb78
Yang <yangluo0901@gmail.com>
2017-12-21 14:51:44 -0800

    assignments of python

--- Python/Django/user_dashboard/apps/dashboard/urls.py
+++ Python/Django/user_dashboard/apps/dashboard/urls.py
@@ -7,9 +7,14 @@ urlpatterns = [
     url(r'register$',views.register_page),
     url(r'signin/process$', views.signin),
     url(r'register/process$', views.register),
-    url(r'dashboard$',views.dashboard),
+    url(r'dashboard/admin$',views.dashboard_admin),
     url(r'user/show/(?P<id>\d+)$',views.show, name = "show"),
     url(r'user/post$', views.post),
     url(r'user/message$',views.message),
+    url(r'dashboard$', views.dashboard),
+    url(r'user/edit/(?P<id>\d+)$',views.edit_page,name = "edit"),
+    url(r'user/edit/process$', views.edit),
+    url(r'user/edit$',views.edit_self_page),
+    url(r'user/destroy/(?P<id>\d+)$', views.remove)
 
 ]
--- Python/Django/user_dashboard/apps/dashboard/views.py
+++ Python/Django/user_dashboard/apps/dashboard/views.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 from .models import *
 from django.shortcuts import render,redirect
 from django.contrib import messages
-# Create your views here.
+import bcrypt
+# page-load functions:
 def home(request):
     return render(request,'dashboard/home.html')
 
@@ -11,6 +12,31 @@ def signin_page(request):
     return render(request,'dashboard/signin.html')
 def register_page(request):
     return render(request,'dashboard/register.html')
+def dashboard(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'dashboard/dashboard.html',context)
+def dashboard_admin(request):
+
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'dashboard/dashboard_admin.html',context)
+
+def edit_page(request, id):
+    context = {
+        'user': User.objects.get(id = id)
+    } # 'user' here: the user whose information will be modified
+    return render(request, 'dashboard/edit_page.html',context)
+
+def edit_self_page(request): # user can only edit theirselves
+    context = {
+        'user' : User.objects.get(id = request.session['user_id'])
+    }
+    return render(request, 'dashboard/edit_self_page.html', context)
+    
+# handlers:
 
 def signin(request):
     result = User.objects.login_validator(request.POST)
@@ -20,8 +46,12 @@ def signin(request):
         return redirect('/signin')
     else:
         request.session['user_id'] = result.id
-        request.session['login_user'] = result.first_name
-    return redirect('/dashboard')
+        request.session['login_user'] = result.first_name # store login user information inside session
+                                                        # so that these information can be used for rendering page
+        if result.level == 9: # admin
+            return redirect('/dashboard/admin')
+        else:
+            return redirect('/dashboard')
 
 def register(request):
     result = User.objects.register_validator(request.POST)
@@ -34,14 +64,6 @@ def register(request):
         result.save()
 
     return redirect('/signin')
-
-
-def dashboard(request):
-
-    context = {
-        'users': User.objects.all()
-    }
-    return render(request, 'dashboard/dashboard.html',context)
 
 def show(request, id):
     context = {
@@ -64,3 +86,42 @@ def message(request):
     # print " i am here"
     Message.objects.create(user = User.objects.get(id =request.session['user_id']),content = request.POST.get('message'), post = Post.objects.get(id = request.POST.get('post_id')))
     return redirect('show', id = request.session['recipient_id'])
+
+
+
+def edit(request):
+    a = User.objects.get(id = request.POST['target_id'])
+    if request.POST.get('first_name'):
+        a.first_name = request.POST.get('first_name')
+    if request.POST.get('last_name'):
+        a.last_name = request.POST.get('last_name')
+    if request.POST.get('email'):
+        a.email = request.POST.get('email')
+    if request.POST.get('level'):
+        if request.POST.get('level') == "Normal":
+            a.level = 1
+        else:
+            a.level = 9
+    try:
+        if request.POST['password'] and request.POST['con_pw']:
+            if request.POST['password'] == request.POST['con_pw']:
+                a.password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt(15))
+            else:
+                messages.error(request,"Password do no match, try it again")
+                return redirect('eidt', id = request.POST.get('target_id'))
+
+        else:
+            messages.error(request,"Please type in password")
+            return redirect('edit', id = request.POST.get('target_id'))
+            print " i am here"
+    except:
+        pass
+    a.save()
+    return redirect('/dashboard/admin')
+
+def remove(request,id):
+    a = User.objects.get(id = id)
+    a.delete()
+    if len(User.objects.all()) == 0:
+        return redirect('/signin') # return back to sign in page, when all records are deleted
+    return redirect('/dashboard/admin')
