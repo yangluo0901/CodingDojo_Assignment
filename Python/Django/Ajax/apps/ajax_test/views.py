2367e12a3d894c5a354f7f29c3553775e9abe118
Yang <yangluo0901@gmail.com>
2018-01-24 10:31:36 -0800

    Ajax2

--- Python/Django/Ajax/apps/ajax_test/views.py
+++ Python/Django/Ajax/apps/ajax_test/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render,redirect
 def index(request):
     return render(request, "ajax_test/index.html")
 def find(request):
-    print request.POST['name']
+
     users=User.objects.filter(name__contains=request.POST['name'])
-    return render(request, "ajax_test/index.html",{'users':users})
+    
+    return render(request, "ajax_test/index.html",{'users':users[0]})
